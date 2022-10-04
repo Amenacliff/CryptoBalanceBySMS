dcc3fa84bf6e8a66f4eff15dc50f6e6f5a9ec73d
Amenacliff <amenaclifford@gmail.com>
2022-10-04 12:21:16 +0100

    Worked on EVM Service EthClient Init

--- main.go
+++ main.go
@@ -1,5 +1,12 @@
 package main
 
+import (
+	"CryptoBalanceBySMS/constants"
+	"CryptoBalanceBySMS/utils/envUtils"
+)
+
 func main() {
-	//envUtil := envUtils.InitEnvUtil()
+	envUtil := envUtils.InitEnvUtil()
+	rpcLink, _ := envUtil.GetEnv(constants.INFURA_ETH_LINK)
+
 }
--- services/evmService/main.go
+++ services/evmService/main.go
@@ -1,6 +1,21 @@
 package evmService
 
+import (
+	"github.com/ethereum/go-ethereum/ethclient"
+	"log"
+)
+
 type EvmService struct {
+	client *ethclient.Client
 }
+
+func InitEvmService(rpcProvider string) *EvmService {
+	client, err := ethclient.Dial(rpcProvider)
+	if err != nil {
+		log.Println(err.Error(), "Unable to dial Client")
+	}
+	return &EvmService{client: client}
+}
+
 type IEvmService interface {
 }
